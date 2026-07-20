ac55d8419c659b79ea26a2c8f9309eaa459acdde
kundalik-qalogs <qalogs.ai@gmail.com>
2026-07-20 16:24:31 +0530

    Add product page test cases and implement product data structure
    - Created test cases for verifying product page elements, total product count, and individual product details.
    - Added JSON data files for product details and page data.
    - Updated ProductsPage and LoginPage classes with necessary locators.

--- pom/ProductsPage.js
+++ pom/ProductsPage.js
@@ -1,8 +1,39 @@
 class ProductsPage {
+  /** @param {import('@playwright/test').Page} page */
   constructor(page) {
     this.page = page;
+
+    // headings
+    this.appLogoLoc = page.locator("div.app_logo");
+    this.hamburgerIconLoc = page.getByRole("button", { name: "Open Menu" });
+
+    //message locators
     this.pageHeadingTxtLoc = page.getByText("Products");
+
+    // Carts locator
+    this.cartIcon = page.locator(".shopping_cart_link");
+    this.shoppingCartBadgeLoc = page.locator(".shopping_cart_badge");
+
+    // products sorting locators
+    this.productSortLoc = page.getByRole("combobox");
+    this.productNameLoc = page.getByTestId("inventory-item-name");
+    this.productPriceLoc = page.getByTestId("inventory-item-price");
+    this.productDescLoc = page.getByTestId("inventory-item-desc");
+
+    //prodcuts locators
+    this.productsLoc = page.locator(".inventory_item");
+
+    // footer
+    this.socialIcons = page.locator("ul.social li");
+    this.socialIcons2 = page.locator("ul.social li a");
+    this.footerCopyRightLoc = page.locator("div.footer_copy");
   }
+
+  async productsPageHeading() {
+    return this.productsPageHeading;
+  }
+
+  async compareProdcutsDetaisl(productName, productPrice, productDescription) {}
 }
 
 export default ProductsPage;
--- tests/productsTest.spec.js
+++ tests/productsTest.spec.js
@@ -1,14 +1,104 @@
 import { test, expect } from "@playwright/test";
+import LoginPage from "../pom/LoginPage";
+import ProductsPage from "../pom/ProductsPage";
+import loginData from "../test-data/loginPage-data.json";
+import productsData from "../test-data/productsPage-data.json";
+import productsListData from "../test-data/productsList-data.json";
+
+const data = { ...loginData, ...productsData };
+
+/** @type {LoginPage} */
+let loginPage;
+/** @type {ProductsPage} */
+let productsPage;
 
 test.describe("handling Products ", () => {
-  test("add product to cart", async ({ page }) => {});
-});
+  test.beforeEach(async ({ page }) => {
+    loginPage = new LoginPage(page);
+    productsPage = new ProductsPage(page);
 
-/**
- * Test cases
- * 1. Add products to carts
- * 2. Verify products present on page
- * 3. check product price after sorting lohi
- * 4. Sort product and check product price
- * 5. Count number of products present
- */
+    loginPage.launchUrl(data.appUrl);
+  });
+
+  test("verify product page elements load successfully @products @smoke @regression", async ({
+    page,
+  }) => {
+    // login to app
+    await loginPage.validLogin(data.username, data.password);
+
+    // assert user is navigated to products page
+    await page.waitForURL(productsData.productsPageUrl);
+
+    // assert applogo text presents
+    await expect.soft(productsPage.appLogoLoc).toHaveText(data.appLogoText);
+
+    // assert cart icon isVisible
+    await expect.soft(productsPage.cartIcon).toBeVisible();
+
+    // assert sorting dropdown isVisible
+    await expect.soft(productsPage.productSortLoc).toBeVisible();
+
+    // assert 6 products cards presents
+    await expect(productsPage.productsLoc).toHaveCount(6);
+
+    // assert footer text matches
+    await expect
+      .soft(productsPage.footerCopyRightLoc)
+      .toHaveText(data.footerCopyRightText);
+
+    // Assert 3 social icons present
+    await expect(productsPage.socialIcons).toHaveCount(3);
+    const socialIconsTexts = await productsPage.socialIcons.allTextContents();
+
+    expect(socialIconsTexts).toEqual(data.socialIconNames);
+  });
+
+  test(
+    "verify_total_product_count_matches_expected_data",
+    { tag: ["@smoke", "@regression"] },
+    async ({ page }) => {
+      // login to app
+      await loginPage.validLogin(data.username, data.password);
+
+      // wait for login and navigate to products apge
+      await page.waitForURL(data.productsPageUrl);
+
+      await expect(productsPage.productsLoc).toHaveCount(6);
+    },
+  );
+
+  test(`verify_individual_product_details_against_data_source`, async ({
+    page,
+  }) => {
+    // login to app
+    await loginPage.validLogin(data.username, data.password);
+
+    // wait for login and navigate to products apge
+    await page.waitForURL(data.productsPageUrl);
+
+    // get all products names, price, descriptions
+    const allProductsNames =
+      await productsPage.productNameLoc.allTextContents();
+    const allProductsDesc = await productsPage.productDescLoc.allTextContents();
+    const allProductsPrices =
+      await productsPage.productPriceLoc.allTextContents();
+
+    //approach 01 - assert products details
+    for (let i = 0; i < productsListData.length; i++) {
+      expect(allProductsNames[i]).toEqual(productsListData[i].productName);
+      expect(allProductsPrices[i]).toEqual(productsListData[i].prodcutPrice);
+      expect(allProductsDesc[i]).toEqual(
+        productsListData[i].productsDescription,
+      );
+    }
+
+    //approach 02 - assert products details
+    const actualProducts = allProductsNames.map((name, i) => ({
+      productName: name,
+      prodcutPrice: allProductsPrices[i],
+      productsDescription: allProductsDesc[i],
+    }));
+
+    expect(actualProducts).toEqual(productsListData);
+  });
+});
